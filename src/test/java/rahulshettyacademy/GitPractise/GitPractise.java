3e52297464e6e962bc740030d2f574de8a5d8f5a
naveen9337 <devrohnav@gmail.com>
2024-05-09 15:10:27 +0530

    first commit

---
+++ src/test/java/rahulshettyacademy/GitPractise/GitPractise.java
@@ -0,0 +1,16 @@
+package rahulshettyacademy.GitPractise;
+
+import org.testng.annotations.Test;
+
+public class GitPractise {
+	
+	@Test
+	
+	public void hello(){
+		
+		System.out.println("Hello Word");
+		
+		
+	}
+
+}
