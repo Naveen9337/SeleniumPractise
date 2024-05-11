40de695c2ba5aef7078680dd345a36e4fe4563fb
Naveen9337 <devrohnav@gmail.com>
2024-05-11 13:59:07 +0530

    Architect 2 has committed'

--- src/test/java/rahulshettyacademy/GitPractise/GitPractise.java
+++ src/test/java/rahulshettyacademy/GitPractise/GitPractise.java
@@ -12,8 +12,34 @@ public class GitPractise {
 		System.out.println("New Added Code");
 		System.out.println("Hello Word1");
 		System.out.println("New Added Code2");
+		System.out.println("Inside Develop Branch By Architect");
 		
 		
 	}
 
+@Test
+	
+	public void hello1(){
+		
+		System.out.println("Hello Word By Architect");
+		System.out.println("New Added Code By Architect");
+		System.out.println("Hello Word1 By Architect");
+		System.out.println("New Added Code2 By Architect");
+		System.out.println("Inside Develop Branch By Architect");
+		
+		
+	}
+
+@Test
+
+public void hello2(){
+	
+	System.out.println("Hello Word By Architect1");
+	System.out.println("New Added Code By Architect1");
+	System.out.println("Hello Word1 By Architect1");
+	System.out.println("New Added Code2 By Architect1");
+	System.out.println("Inside Develop Branch By Architect1");
+	
+	
+}
 }
