b0bab4ff808015ee5d8bf078cc555115c56ac8b2
Naveen9337 <devrohnav@gmail.com>
2024-05-11 14:45:35 +0530

    Switched to Master

---
+++ src/test/java/rahulshettyacademy/GitPractise/Architectcode.java
@@ -0,0 +1,14 @@
+package rahulshettyacademy.GitPractise;
+
+import org.testng.annotations.Test;
+
+public class Architectcode {
+	
+	@Test
+	
+	public void Arch()
+	{
+		System.out.println("This is code added by original Architect");
+	}
+
+}
